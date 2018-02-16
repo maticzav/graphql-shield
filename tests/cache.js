604fe5738f2f6eac66212a6177596a5e56441e33
Matic Zavadlal <matic.zavadlal@gmail.com>
2018-02-16 17:45:54 +0100

    Cache

--- tests/cache.js
+++ tests/cache.js
@@ -1,22 +1,69 @@
 import test from 'ava'
+import { graphql } from 'graphql'
+import { makeExecutableSchema } from 'graphql-tools'
 import { shield } from '../dist/src/index.js'
 
 // Setup
+const _users = [
+   { id: '1', name: 'user1', secret: 'secret1' },
+   { id: '2', name: 'user2', secret: 'secret2' },
+   { id: '3', name: 'user3', secret: 'secret3' },
+]
 
-const resolvers = {
+const _typeDefs = `
+   type User {
+      id: String!
+      name: String!
+      secret: String!
+   }
+
+   type Query {
+      users: [User!]!
+   }
+
+   schema {
+      query: Query
+   }
+`
+
+const _resolvers = {
    Query: {
-      hello: (_, { name }) => `Hello ${name || 'World'}`,
-      secret: (_, { agent }) => `Hello agent ${agent}`,
+      users: () => users
    },
 }
 
-const permissions = {
+const auth = t => (_, { code }) => {
+   t.pass()
+   return code === 'code'
+}
+
+const _permissions = {
    Query: {
-      hello: () => true,
-      secret: (_, { code }) => code === 'donttellanyone'
+      users: auth
+   },
+   User: {
+      secret: auth
    }
 }
 
+const setup = (options) => makeExecutableSchema({
+   typeDefs: _typeDefs,
+   resolvers: shield(_resolvers, _permissions, options)
+})
+
+
 // Tests
 
-test.todo('Cache test')
+test('Working cache.', async t => {
+   const schema = setup()
+
+   const query = `{
+      users(code: "code") {
+
+      }
+   }`
+
+   t.pass()
+})
+
+test.todo('No caching when disabled.')
