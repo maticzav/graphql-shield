7d6730e7d0fd6b9696896873d580a49494bbe73c
Jonathan Van Buren <vanbujm@gmail.com>
2019-06-20 11:34:21 +0800

    Fixed lint issues

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
 export { IRules, IRule } from './types'
 export { shield } from './shield'
-export { rule, inputRule, allow, deny, and, or, not, chain } from './constructors'
+export {
+  rule,
+  inputRule,
+  allow,
+  deny,
+  and,
+  or,
+  not,
+  chain,
+} from './constructors'
