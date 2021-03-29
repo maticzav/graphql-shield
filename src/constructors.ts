c4ac4f5f25fd04a8644f6adeadbde9fa899aa7ac
Kevin Ingersoll <kevin@kenchi.com>
2021-03-29 11:31:48 -0700

    Upgrade yup to 0.32

--- src/constructors.ts
+++ src/constructors.ts
@@ -1,4 +1,5 @@
 import * as Yup from 'yup'
+import { ValidateOptions } from 'yup/lib/types'
 import {
   IRuleFunction,
   IRuleConstructorOptions,
@@ -72,10 +73,11 @@ export const rule = (
  * @param schema
  */
 export const inputRule = <T>(name?: string) => (
-  schema: (yup: typeof Yup, ctx: IShieldContext) => Yup.Schema<T>,
-  options?: Yup.ValidateOptions,
+  schema: (yup: typeof Yup, ctx: IShieldContext) => Yup.SchemaOf<T>,
+  options?: ValidateOptions,
 ) => {
   if (typeof name === 'string') {
+    // @ts-expect-error
     return new InputRule(name, schema, options)
   } else {
     return new InputRule(Math.random().toString(), schema, options)
