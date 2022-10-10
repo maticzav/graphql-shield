ec7367517927df148a2f6555ebefdd1d5caf6698
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2022-10-10 03:23:53 +0200

    dummy (not really) commit (:

--- website/src/pages/_app.tsx
+++ website/src/pages/_app.tsx
@@ -1,6 +1,7 @@
+import { ReactElement } from 'react'
 import { AppProps } from 'next/app'
 import '@theguild/components/style.css'
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps): ReactElement {
   return <Component {...pageProps} />
 }
